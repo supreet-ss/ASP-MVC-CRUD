8ca6e40d83dc36647baa9877f21987c711cae08e
Supreet <supreetsup36@gmail.com>
2023-01-09 08:50:40 +0530

    Adding my project to github

---
+++ Full_Stack_API/Full_Stack_Web_API/Full_Stack_Web_API/Data/FullStackDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Full_Stack_Web_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Full_Stack_Web_API.Data
+{
+    public class FullStackDbContext : DbContext
+    {
+        public FullStackDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees{ get; set; }
+
+    }
+}
---
+++ Full_Stack_API/Full_Stack_Web_API/Full_Stack_Web_API/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿namespace Full_Stack_Web_API.Models
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public long Phone { get; set; }
+        public long Salary { get; set; }
+
+        public string Department { get; set;}
+
+    }
+}
